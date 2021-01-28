55bc1dd70d6f0a9838f2c48f54279b52dc91f214
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-01-28 19:33:06 +0530

    Add files via upload
    Updated the code

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,47 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+	
+    public static Stats getStatistics(List<Float> numbers) {
+    	float avg;
+    	int count = 0;
+    	float total = 0;
+    	Float min = Float.NaN;
+    	Float max = Float.NaN;
+    	Stats s = new Stats();
+    	for(Float num: numbers)
+    	{
+    		if(count == 0)
+    		{
+    			min = num;
+    			max = num;
+    		}
+    		if(num>max)
+    		{
+    			max = num; 
+    		}
+    		if(num<min)
+    		{
+    			min = num;
+    		}
+    		total = total + num;
+    		
+			count++;
+    	}
+    
+    	avg = total/count;	
+    	s.average=avg;
+    	s.min=min;
+    	s.max=max;
+       	return s;
+    	
+    	
+    }
+    public static class Stats
+    {
+    	public float average;
+		public float min;
+		public float max;
+	
     }
 }
